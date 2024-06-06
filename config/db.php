807357b1fe385f7a0fac982b81e57e52a3735c73
Brian Mwengi <mwengibrian@gmail.com>
2024-06-06 09:08:04 +0300

    Update with comment lines

--- config/db.php
+++ config/db.php
@@ -1,8 +1,10 @@
 <?php
+// Display errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Database connection details
 $servername = "localhost";
 $username = "root";
 $password = "";
--- public/setup.php
+++ public/setup.php
@@ -2,3 +2,4 @@
 require_once '../config/db.php';
 
 create_tables($conn);
+
